f9141393f86beb36a9a6f7013e9cd95fb9d197cb
Aluno_Noite (Kerllom) <kerllomsilva@gmail.com>
2026-05-25 21:21:26 -0300

    Implementa classe ConexaoDB para conectar ao MySQL

--- PROGRAMACAO-DE-APLICATIVOS/ToDoList/src/Tarefas/dao/ConexaoDB.java
+++ PROGRAMACAO-DE-APLICATIVOS/ToDoList/src/Tarefas/dao/ConexaoDB.java
@@ -7,20 +7,9 @@ import java.sql.SQLException;
 public class ConexaoDB {
     private static final String URL = "jdbc:mysql://localhost:3306/todolist_db";
     private static final String USUARIO = "root";
-    private static final String SENHA = "1234";
+    private static final String SENHA = "";
 
     public static Connection getConexao() throws SQLException {
         return DriverManager.getConnection(URL, USUARIO, SENHA);
-    }
-
-    public static void main(String[] args) {
-        try {
-            Connection conexao = getConexao();
-            System.out.println("✅ Conexão bem-sucedida com o banco!");
-            conexao.close();
-        } catch (SQLException e) {
-            System.out.println("❌ Erro ao conectar:");
-            e.printStackTrace();
-        }
     }
 }
