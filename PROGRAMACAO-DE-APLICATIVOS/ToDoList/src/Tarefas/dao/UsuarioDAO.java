44a94c45d854d538a5df5fbb16f06a13e75929fb
Aluno_Noite (Kerllom) <kerllomsilva@gmail.com>
2026-05-25 21:41:54 -0300

    Implementa ConexaoDB e metodo inserir do UsuarioDAO

--- PROGRAMACAO-DE-APLICATIVOS/ToDoList/src/Tarefas/dao/UsuarioDAO.java
+++ PROGRAMACAO-DE-APLICATIVOS/ToDoList/src/Tarefas/dao/UsuarioDAO.java
@@ -1,4 +1,23 @@
 package Tarefas.dao;
 
+import Tarefas.model.Usuario;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class UsuarioDAO {
+    public void inserir(Usuario usuario) throws SQLException {
+        String sql = "INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)";
+
+        try (Connection conn = ConexaoDB.getConexao();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, usuario.getNome());
+            stmt.setString(2, usuario.getEmail());
+            stmt.setString(3, usuario.getSenha());
+
+            stmt.executeUpdate();
+        }
+    }
 }
